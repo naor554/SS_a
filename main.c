1752da67485e65d71aff5fa0e24d4386c2507c51
naor554 <naoryoyo@gmail.com>
2022-11-29 23:55:49 +0200

    adding main

--- main.c
+++ main.c
@@ -3,25 +3,21 @@
 
 int main(){
     while(1) {
-        printf("Choose a Function\n");
         char arg;
         scanf("%c" , &arg);
         if(arg == 'A'){
             CreateMatrix();
         }else if (arg == 'B'){
-            printf("Insert Two Edges\n");
             int i;
             int j;
             scanf("%d %d", &i , &j);
             IsTherePath(i , j);
         }else if (arg == 'C'){
-            printf("Insert Two Edges\n");
             int i;
             int j;
             scanf("%d %d", &i , &j);
             ShortestPath(i , j);
         }else if (arg == 'D'){
-            printf("Bye Bye\n");
             break;
         }    
     }
